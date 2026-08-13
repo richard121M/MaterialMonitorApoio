d59660d963d30cf2698cce48c59dd7010933239d
richard121M <richardtavares520@gmail.com>
2026-08-13 14:16:53 -0300

    v0.003

--- python/MultiplicacaoMatris/MultiMatris.py
+++ python/MultiplicacaoMatris/MultiMatris.py
@@ -1,13 +1,43 @@
-#criar um programa que faca a multiplicacao de duas matrizes quadradas
+#crie um programa que faca a multiplicacao de duas matrizes quadradas
 
-size = int(input("digite a orde das matrizes quadradas"))
+def PrintMatris(matris,size):
+    for i in range(size):
+        print(matris[i])
+
+def EscreverMatris(matris,size,nomeMatris):
+    for i in range(size):
+        #criando as linhas dentro do vetor para virar uma matris
+        matris.append([])
+        for j in range(size):
+            valor = int(input(f"{nomeMatris} valor da posicao {i},{j} :"))
+            #adicionando o valor dentro da linha para criar as colunas 
+            matris[i].append(valor)
+
+#Pedindo o valor da ordem da matris
+SIZE = int(input("digite a ordem das matrizes quadradas"))
+
+#Declarando a matris A
 Amatris = []
-for i in range(size):
-    Amatris.append([])
-    for j in range(size):
-        valor = int(input(f"valor da posicao {i},{j} :"))
-        Amatris[i].append(valor)
-
+EscreverMatris(Amatris,SIZE,"Matris A")
 print("Matris A: ")
-for i in range(size):
-    print(Amatris[i])
+PrintMatris(Amatris,SIZE)
+
+#Declarando a matris B
+Bmatris = []
+EscreverMatris(Bmatris,SIZE,"Matris B")
+print("Matris B: ")
+PrintMatris(Bmatris,SIZE)
+
+#Multiplicar
+#Declarando a matris Produto
+Produto = []
+for Va in range(SIZE):
+    Produto.append([])
+    for Vb in range(SIZE):
+        valor = 0
+        for s in range(SIZE):
+            valor += Amatris[Va][s]*Bmatris[s][Vb]
+        Produto[Va].append(valor)
+
+print("O produto de A*B é ")
+PrintMatris(Produto,SIZE)
