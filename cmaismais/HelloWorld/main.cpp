6e7665477008874a6517cc817d93b1adcefdc607
richard121M <richardtavares520@gmail.com>
2026-08-13 13:52:39 -0300

    v0.002

--- cmaismais/HelloWorld/main.cpp
+++ cmaismais/HelloWorld/main.cpp
@@ -3,5 +3,18 @@
 using namespace std;
 
 int main(){
+    //ola mundo no C++
     cout << "Ola mundo" << endl;
+    // O endl no final serve para quebrar a linha
+    //exe,plo sem o endl
+    cout << "ola";
+    cout << " mundo";
+
+    //tambem e possivel faze a quebra de linha pelo \n
+    cout << "\n";
+
+    int idade = 12;
+    cout << "idade e igual a" << idade << endl;
+
+    return 0;
 }
